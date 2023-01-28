c6a46e7b62a1676e2dc7ce8d22c9d46531c8b250
Elisson Rodrigues <elissonrodrigues98@gmail.com>
2023-01-27 22:47:07 -0300

    Update exercicio05.py

--- 2º Período/LISTA07/exercicio05.py
+++ 2º Período/LISTA07/exercicio05.py
@@ -1,36 +1,40 @@
 from prettytable import PrettyTable
 from requests import get
+from traceback import format_exc
 
 while True:
-    sigla = input('\n\nDigite a sigla do seu estado, ex: RN, AC, SP: ').upper()
-    print ()
-    if not all([len(sigla) == 2, sigla.isalpha()]):
-        print('Verifique a sigla do seu estado e tente novamente.')
-    else:
-        req = get(f'http://servicodados.ibge.gov.br/api/v1/localidades/estados/{sigla}/municipios')
-        json = req.json()
-
-        if json:
-            x = PrettyTable(["Municipio", "Microrregião", "mesorregiao", "UF", "Regiao"])
-            x.align["Municipio"] = "l"
-            x.align["Microrregião"] = "c"
-            x.align["UF"] = "l"
-            x.align["Regiao"] = "l"
-
-            x.padding_width = 1
-
-            for y in json:
-                regiao = f"{y['microrregiao']['mesorregiao']['UF']['regiao']['sigla']} - {y['microrregiao']['mesorregiao']['UF']['regiao']['nome']}"
-                municipio = y['nome']
-                microrregiao = y['microrregiao']['nome']
-                mesoregiao = y['microrregiao']['mesorregiao']['nome']
-                uf = f"{y['microrregiao']['mesorregiao']['UF']['sigla']} - {y['microrregiao']['mesorregiao']['UF']['nome']}" 
-
-                x.add_row([municipio, microrregiao, mesoregiao, uf, regiao])
-            
-            print (x)
+    try:
+        sigla = input('\n\nDigite a sigla do seu estado, ex: RN, AC, SP: ').upper()
+        print ()
+        if not all([len(sigla) == 2, sigla.isalpha()]):
+            print('Verifique a sigla do seu estado e tente novamente.')
         else:
-            print ('Verifique a sigla do seu estado e tente novamente.')
-
-
-
+            req = get(f'http://servicodados.ibge.gov.br/api/v1/localidades/estados/{sigla}/municipios')
+            json = req.json()
+    
+            if json:
+                x = PrettyTable(["Municipio", "Microrregião", "Mesorregiao", "UF", "Regiao"])
+                x.align["Municipio"] = "l"
+                x.align["Microrregião"] = "c"
+                x.align["UF"] = "l"
+                x.align["Regiao"] = "l"
+    
+                x.padding_width = 1
+    
+                for y in json:
+                    regiao = f"{y['microrregiao']['mesorregiao']['UF']['regiao']['sigla']} - {y['microrregiao']['mesorregiao']['UF']['regiao']['nome']}"
+                    municipio = y['nome']
+                    microrregiao = y['microrregiao']['nome']
+                    mesoregiao = y['microrregiao']['mesorregiao']['nome']
+                    uf = f"{y['microrregiao']['mesorregiao']['UF']['sigla']} - {y['microrregiao']['mesorregiao']['UF']['nome']}" 
+    
+                    x.add_row([municipio, microrregiao, mesoregiao, uf, regiao])
+                
+                print (x)
+            else:
+                print ('Verifique a sigla do seu estado e tente novamente.')
+    except KeyboardInterrupt:
+        print ("\n\nEncerrando script... Até breve :)")
+        break
+    except:
+        print (format_exc())
