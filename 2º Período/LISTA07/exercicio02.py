ca40327516461789902a7c837985779022b97d52
Elisson Rodrigues <elissonrodrigues98@gmail.com>
2023-01-27 22:48:14 -0300

    Update exercicio02.py

--- 2º Período/LISTA07/exercicio02.py
+++ 2º Período/LISTA07/exercicio02.py
@@ -6,24 +6,25 @@ from traceback import format_exc
 base_url = 'https://viacep.com.br/ws/{}/json'
 
 while True:
-    cep = sub('[-,_/]', '', input('\n\nDigite o seu CEP: ').strip())
-
-    if len(cep) > 8:
-        print ('Cep inválido! Digite um cep valído e tente novamente.')
-    else:
-        try:
+    try:
+        cep = sub('[-,_/]', '', input('\n\nDigite o seu CEP: ').strip())
+    
+        if not len(cep) == 8:
+            print ('\n\nCep inválido! Digite um cep valído e tente novamente.')
+        else:
             req = get(base_url.format(cep))
             
             if req.status_code in [200, 201]:
                 json = req.json()
-
+    
                 if not 'erro' in json.keys():
                     system('cls' if name == 'nt' else 'clear')
                     print ('\n'.join(map(lambda x: f'{x.upper()}: {json[x] if json[x] else "N/A"}', json.keys())))
                 else:
-                    print ('Verifique o cep e tente novamente')
-        except:
-            print (format_exc())
-            
-            
-    
+                    print ('\n\nVerifique o cep e tente novamente')
+                    
+    except KeyboardInterrupt:
+        print ("\n\nEncerrando script... Até breve :)")
+        break
+    except:
+        print (format_exc())
