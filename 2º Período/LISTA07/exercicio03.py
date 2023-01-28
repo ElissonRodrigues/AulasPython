0e57c3e1e39b0cc60a2444a3499a51fdd3de52b0
Elisson Rodrigues <elissonrodrigues98@gmail.com>
2023-01-27 22:47:36 -0300

    Update exercicio03.py

--- 2º Período/LISTA07/exercicio03.py
+++ 2º Período/LISTA07/exercicio03.py
@@ -44,46 +44,55 @@ def main():
         system('cls' if name == "nt" else "clear")
         
         while True:
-            periodo_letivo = input("\n\nInforme o período letivo para consultar deus dados. Exemplo: 2022.2: ")
+            try:
+                periodo_letivo = input("\n\nInforme o período letivo para consultar deus dados. Exemplo: 2022.2: ")
+                
+                if all([len(periodo_letivo.split(".")) == 2, periodo_letivo.replace(".", "").isnumeric()]):
+                    headers={"Authorization": f"Bearer {login_data['access']}"}
+                    req = get(f'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/boletim/{periodo_letivo.split(".")[0]}/{periodo_letivo.split(".")[1]}/', headers=headers)
+                    
+                    x = PrettyTable(["Disciplina", "Frequência", "Faltas", "Notas"])
+        
+                    x.align["Disciplina"] = "l"
+                    x.align["Frequência"] = "c"
+                    x.align["Notas"] = "c"
+                    
+                    x.padding_width = 1
+                    
+                    print ()
+                    for boletim in req.json():
+                        nota1 = str(boletim["nota_etapa_1"]["nota"]) if boletim["nota_etapa_1"]["nota"] else '00'
+                        nota2 = str(boletim["nota_etapa_2"]["nota"]) if boletim["nota_etapa_2"]["nota"] else '00'
+                        nota3 = str(boletim["nota_etapa_3"]["nota"]) if boletim["nota_etapa_3"]["nota"] else '00'
+                        nota4 = str(boletim["nota_etapa_4"]["nota"]) if boletim["nota_etapa_4"]["nota"] else '00'
+                       
+                        numero_faltas = str(boletim["numero_faltas"]).zfill(2) if boletim["numero_faltas"] else '00'
+                        frequencia = str(round(boletim["percentual_carga_horaria_frequentada"])).zfill(2)
+                        
+                        notas = ", ".join(map(lambda y: y, [nota1, nota2, nota3, nota4][:boletim["quantidade_avaliacoes"]]))
+                        disciplina = boletim["disciplina"]
+                        
+                        x.add_row([disciplina, frequencia, numero_faltas, notas])
+                        
+                    print (x)
+                    
+                    escolha = input("\n\nFazer uma nova consulta?\n\nS - SIM\nN - NÃO\n\n>>> ")
+                    
+                    if escolha.upper() in ["S", "SIM"]: print ("\n\n")
+                    else: break
+                    
+                else:
+                    print ("\n\nVerifique o período letivo informado e tente novamente!")
             
-            if all([len(periodo_letivo.split(".")) == 2, periodo_letivo.replace(".", "").isnumeric()]):
-                headers={"Authorization": f"Bearer {login_data['access']}"}
-                req = get(f'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/boletim/{periodo_letivo.split(".")[0]}/{periodo_letivo.split(".")[1]}/', headers=headers)
+            except KeyboardInterrupt:
+                print ("\n\nEncerrando script... Até breve :)")
+                break
                 
-                x = PrettyTable(["Disciplina", "Frequência", "Faltas", "Notas"])
-    
-                x.align["Disciplina"] = "l"
-                x.align["Frequência"] = "c"
-                x.align["Notas"] = "c"
+            except:
+                print (format_exc())
                 
-                x.padding_width = 1
-                
-                print ()
-                for boletim in req.json():
-                    nota1 = str(boletim["nota_etapa_1"]["nota"]) if boletim["nota_etapa_1"]["nota"] else '00'
-                    nota2 = str(boletim["nota_etapa_2"]["nota"]) if boletim["nota_etapa_2"]["nota"] else '00'
-                    nota3 = str(boletim["nota_etapa_3"]["nota"]) if boletim["nota_etapa_3"]["nota"] else '00'
-                    nota4 = str(boletim["nota_etapa_4"]["nota"]) if boletim["nota_etapa_4"]["nota"] else '00'
-                   
-                    numero_faltas = str(boletim["numero_faltas"]).zfill(2) if boletim["numero_faltas"] else '00'
-                    frequencia = str(round(boletim["percentual_carga_horaria_frequentada"])).zfill(2)
-                    
-                    notas = ", ".join(map(lambda y: y, [nota1, nota2, nota3, nota4][:boletim["quantidade_avaliacoes"]]))
-                    disciplina = boletim["disciplina"]
-                    
-                    x.add_row([disciplina, frequencia, numero_faltas, notas])
-                    
-                print (x)
-                
-                escolha = input("\n\nFazer uma nova consulta?\n\nS - SIM\nN - NÃO\n\n>>> ")
-                
-                if escolha.upper() in ["S", "SIM"]: print ("\n\n")
-                else: break
-                
-            else:
-                print ("\n\nVerifique o período letivo informado e tente novamente!")
     elif logado == False:
-        print ("\n\nInfelizmente não foi possível validar suas informações, tente novamente mais tarde!")
+        print ("\n\nInfelizmente não foi possível validar suas informações, tente novamente mais tarde!\n\n")
     else:
         print ("\n\nObrigado por utilizar o script SUAP. Até Mais!")
 
