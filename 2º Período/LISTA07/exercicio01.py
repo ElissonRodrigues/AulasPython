76b75bf8eac70999b41e5104b97f5740116f15e7
Elisson Rodrigues <elissonrodrigues98@gmail.com>
2023-02-06 11:41:53 -0300

    Update exercicio01.py

--- 2º Período/LISTA07/exercicio01.py
+++ 2º Período/LISTA07/exercicio01.py
@@ -1,8 +1,15 @@
 from requests import get
 from traceback import format_exc
 
-base_url = "https://dicio-api-ten.vercel.app/v2/"
+def get_word_info(word, endpoint):
+    url = f"https://dicio-api-ten.vercel.app/v2/{endpoint}/{word}"
+    response = get(url)
 
+    if response.status_code in [200, 201]:
+        return response.json()
+    else:
+        return None
+        
 while True:
     try:
         palavra = input("\n\nDigite uma palavra para encontrar seu significado: ")
@@ -10,37 +17,24 @@ while True:
         if len(palavra.split()) > 1:
             print("\n\nVocê digitou mais de uma palavra, tente novamente digitando apenas uma palavra: ")
         else:
-            req_sig = get(f"{base_url}significados/{palavra}")
-
-            if req_sig.status_code in [200, 201]:
-                json = req_sig.json()
-
-                if not json[0]["meanings"]:
-                    print(f'\n\nSem significado para: "{palavra}"')
-                else:
-                    signifidos = "\n".join(map(lambda x: f"{x[0]+1} - {x[1]}", enumerate(json[0]["meanings"])))
-                    print(f"\n\nSignificado:\n{signifidos}") if signifidos else ""
-                    print(f'\n\nEtimologia:\n{json[0]["etymology"]}') if json[0]["etymology"] else ""
-
-                    endpoints = [f"{base_url}sinonimos/{palavra}", f"{base_url}silabas/{palavra}"]
-
-                    for url in endpoints:
-                        req = get(url)
-
-                        if req.status_code in [200, 201]:
-                            json = req.json()
-
-                            resultado = "\n".join(map(lambda x: f"{x}", json))
-
-                            if "sinonimos" in url:
-                                print(f"\n\nSinonimos:\n{resultado}") if resultado else ""
-                            else:
-                                print(f"\n\nSilabas:\n{resultado}") if resultado else ""
-
+            meanings = get_word_info(palavra, 'significados')
+            
+            if not meanings:
+                print(f'\n\nSem significado para: "{palavra}"')
             else:
-                print("\n\nPalavra não encontrada, use outra palavra")
+                significados= "\n".join(f"{i + 1} - {meaning}" for i, meaning in enumerate(meanings[0]["meanings"]))
+                print(f"\n\nSignificado:\n{significados}") if significados else ""
+                print(f'\n\nEtimologia:\n{meanings[0]["etymology"]}') if meanings[0]["etymology"] else ""
+
+                sinonimos = get_word_info(palavra, "sinonimos")
+                print(f"\nSinônimos:\n{', '.join(sinonimos)}") if sinonimos else ""
+                
+                silabas = get_word_info(palavra, "silabas")
+                print(f"\nSílabas:\n{', '.join(silabas)}") if silabas else ""
+                
     except KeyboardInterrupt:
         print("\n\nScript encerrado. Até breve!")
         break
     except:
         print(format_exc())
+        
